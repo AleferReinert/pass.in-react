cc7c7208e139972d1fe22beb0ec20395d0b5af64
Alefer Reinert <aleferreinert@gmail.com>
2025-07-07 12:17:01 -0300

    fix(Header): add alt on logo image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,45 +1,45 @@
 import { useContext } from 'react'
-import { SearchProps } from './Search'
-import NlwUniteIcon from '/nlw-united-icon.svg'
 import { PageContext } from '../App'
 import { useUrl } from '../hooks/useUrl'
+import { SearchProps } from './Search'
+import NlwUniteIcon from '/nlw-united-icon.svg'
 
 export interface HeaderProps extends Pick<SearchProps, 'setSearch'> {
-    activeTab: string
-    setActiveTab: React.Dispatch<React.SetStateAction<"events" | "attendees">>
+	activeTab: string
+	setActiveTab: React.Dispatch<React.SetStateAction<'events' | 'attendees'>>
 }
 
 export function Header({ activeTab, setActiveTab, setSearch }: HeaderProps) {
-    const { setPage } = useContext(PageContext)
-    const { updateUrlParams } = useUrl()
-    const active = activeTab === 'events' ? 'first:*:text-white' : 'last:*:text-white'
+	const { setPage } = useContext(PageContext)
+	const { updateUrlParams } = useUrl()
+	const active = activeTab === 'events' ? 'first:*:text-white' : 'last:*:text-white'
 
-    function changeToEvents() {
-        setActiveTab('events') 
-        setPage(1)
-        setSearch('')
-        updateUrlParams({
-            search: null,
-            page: null
-        })
-    }
-    
-    function changeToAttendees() {
-        setActiveTab('attendees')
-    }
+	function changeToEvents() {
+		setActiveTab('events')
+		setPage(1)
+		setSearch('')
+		updateUrlParams({
+			search: null,
+			page: null
+		})
+	}
 
-    return (
-        <header className='flex items-center gap-5 pt-4 pb-7 sm:pt-7 text-zinc-400'>
-            <img src={NlwUniteIcon} />
-            
-            <nav className={'flex gap-5 font-medium text-sm ' + active}>
-                <button className='focus-visible:outline-none' onClick={changeToEvents}>
-                    Eventos
-                </button>
-                <button className='focus-visible:outline-none' onClick={changeToAttendees}>
-                    Participantes
-                </button>
-            </nav>
-        </header>
-    )
+	function changeToAttendees() {
+		setActiveTab('attendees')
+	}
+
+	return (
+		<header className='flex items-center gap-5 pt-4 pb-7 sm:pt-7 text-zinc-400'>
+			<img src={NlwUniteIcon} alt='pass.in' />
+
+			<nav className={'flex gap-5 font-medium text-sm ' + active}>
+				<button className='focus-visible:outline-none' onClick={changeToEvents}>
+					Eventos
+				</button>
+				<button className='focus-visible:outline-none' onClick={changeToAttendees}>
+					Participantes
+				</button>
+			</nav>
+		</header>
+	)
 }
